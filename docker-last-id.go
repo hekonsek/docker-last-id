7cdb9efa9caa9c930e82811ca16087641a3eeb19
Henryk Konsek <hekonsek@gmail.com>
2020-08-07 14:40:59 +0200

    Added "--dockerized" option.

--- docker-last-id.go
+++ docker-last-id.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 	"strings"
 )
 import "github.com/hekonsek/osexit"
 
 func main() {
-	var output, err = exec.Command("docker", "ps", "-a", "-n1").CombinedOutput()
+	offset := 1
+	dockerized := len(os.Args) > 1 && os.Args[1] == "--dockerized"
+	if dockerized {
+		offset = 2
+	}
+	var output, err = exec.Command("docker", "ps", "-a", "-n2").CombinedOutput()
 	osexit.ExitOnError(err)
 	var outputLines = strings.Split(string(output), "\n")
-	var lastId = strings.Split(outputLines[1], " ")[0]
+	var lastId = strings.Split(outputLines[offset], " ")[0]
 	fmt.Println(lastId)
 }
